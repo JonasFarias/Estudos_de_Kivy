76d527811f6b680514480319c0a441b4bf4a0e5f
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-08-26 21:10:00 -0300

    .

---
+++ Aula 07/main.py
@@ -0,0 +1,21 @@
+import kivy
+kivy.require('1.9.1')
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MinhaTela(BoxLayout):
+
+    def click(self):
+        print('oi')
+        self.ids.lb1.text = text = 'teste'
+        self.ids.lb2.text = text = '10'
+
+
+
+class Estudo4App(App):
+    pass
+
+janela = Estudo4App()
+janela.run()
+
