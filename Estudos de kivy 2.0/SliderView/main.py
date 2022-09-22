5b6b4c2e471da723413c53a775c0b9e0ac2c1320
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-09-21 23:02:07 -0300

    Scroview

---
+++ Estudos de kivy 2.0/SliderView/main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
+
+
+class Scroller_Func():
+    pass
+
+class Stack(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(100):
+            b1 = Button(text=str(i+1), size_hint=(None, None), size=(100, 100))
+            self.add_widget(b1)
+
+
+
+# App Creation
+class TestApp(App):
+    pass
+
+
+TestApp().run()
