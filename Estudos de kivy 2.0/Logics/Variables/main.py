589fa65dc80b9c3e31cab4c9089cf70d25878e4d
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-09-25 17:00:05 -0300

    .

--- Estudos de kivy 2.0/Logics/Variables/main.py
+++ Estudos de kivy 2.0/Logics/Variables/main.py
@@ -1,6 +1,17 @@
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+
+
+
+
+class Variables(BoxLayout):
+    _text_ = StringProperty('Helo Word')
+
+    def Pressing(self):
+        self._text_ = ('Welcome')
+
 
 
 class LogicalInterface(BoxLayout):
