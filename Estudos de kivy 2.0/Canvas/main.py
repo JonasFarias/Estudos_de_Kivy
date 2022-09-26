da607ec285c1b6aa67d8b46cc17c3db0082aa9e3
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-09-26 20:30:12 -0300

    .

--- Estudos de kivy 2.0/Canvas/main.py
+++ Estudos de kivy 2.0/Canvas/main.py
@@ -5,42 +5,14 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
 
-Builder.load_file("other.kv")
-
-
-class ExternalKv(BoxLayout):
+class Canvas_Tuts(BoxLayout):
     pass
 
 
-class Variables(BoxLayout):
-    _text_ = StringProperty('Helo Word')
 
-    def Pressing(self):
-        self._text_ = ('Welcome')
-
-
-
-class LogicalInterface(BoxLayout):
-    def OnPressing(self, ID, input):
-        print('Oi')
-        ID.text = input.text
-
-    def OnRelease(self, ID, input):
-        print('ah')
-
-
-class Binding(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        b1 = Button(text='Texto')
-        b1.bind(on_press=self.callback_funt)
-        self.add_widget(b1)
-    def callback_funt(self, event):
-        print('Foi')
 
 class TestApp(App):
-    def build(self):
-        return Binding()
+    pass
 
 
 TestApp().run()
