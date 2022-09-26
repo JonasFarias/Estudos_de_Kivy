439b8994c44e3107c53ebaf2c50d4b4d5638e984
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-09-26 16:57:01 -0300

    kivy

---
+++ Estudos de kivy 2.0/Logics/Builder/main.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+
+Builder.load_file("other.kv")
+
+
+class ExternalKv(BoxLayout):
+    pass
+
+
+class Variables(BoxLayout):
+    _text_ = StringProperty('Helo Word')
+
+    def Pressing(self):
+        self._text_ = ('Welcome')
+
+
+
+class LogicalInterface(BoxLayout):
+    def OnPressing(self, ID, input):
+        print('Oi')
+        ID.text = input.text
+
+    def OnRelease(self, ID, input):
+        print('ah')
+
+
+
+
+class TestApp(App):
+    def build(self):
+        return ExternalKv()
+
+
+TestApp().run()
